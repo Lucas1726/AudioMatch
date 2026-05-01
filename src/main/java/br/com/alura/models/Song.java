9bdd9c304ec3435b4df4529fec3a26c1190ad13f
Lucas Gonçalves <92900328+Lucas1726@users.noreply.github.com>
2026-05-01 02:46:26 -0300

    Implement Song class with properties and methods

--- src/main/java/br/com/alura/models/Song.java
+++ src/main/java/br/com/alura/models/Song.java
@@ -1,4 +1,46 @@
 package br.com.alura.models;
 
-public class Song {
+public class Song extends Audio implements Classifiable {
+    private String album;
+    private String artista;
+    private String genero;
+
+    @Override
+    public int getClassificacao() {
+        if (this.getTotalReproducoes() >= 2000) {
+            return 10;
+        } else  {
+            return 7;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Música: " + this.getTitulo() + " | Artista: " + this.getArtista() +
+                " | Classificação: " + this.getClassificacao();
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
