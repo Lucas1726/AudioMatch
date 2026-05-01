1d3307021fa08b4f3015b544eabab6602a935bfa
Lucas Gonçalves <92900328+Lucas1726@users.noreply.github.com>
2026-05-01 02:45:11 -0300

    Implement Audio class with properties and methods
    Added fields and methods to manage audio properties.

--- src/main/java/br/com/alura/models/Audio.java
+++ src/main/java/br/com/alura/models/Audio.java
@@ -1,4 +1,35 @@
 package br.com.alura.models;
 
 public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+
+    public void curte(){
+        this.totalCurtidas++;
+    }
+
+    public void reproduz(){
+        this.totalReproducoes++;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassificacao() {
+        return 0;
+    }
 }
