38f7408475c99e02b155764e23cde75d0f4ed5f4
Lucas Gonçalves <92900328+Lucas1726@users.noreply.github.com>
2026-05-01 02:45:44 -0300

    Add method to include classified items in favorites

--- src/main/java/br/com/alura/models/Favorites.java
+++ src/main/java/br/com/alura/models/Favorites.java
@@ -1,4 +1,11 @@
 package br.com.alura.models;
 
 public class Favorites {
+    public void inclui(Classifiable item) {
+        if (item.getClassificacao() >= 9) {
+            System.out.println("Este conteúdo é um sucesso absoluto!");
+        } else {
+            System.out.println("Este conteúdo está sendo muito bem avaliado!");
+        }
+    }
 }
