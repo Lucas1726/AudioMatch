03077171ad8533cb132a7456fdbbc8925909ea4c
Lucas Gonçalves <92900328+Lucas1726@users.noreply.github.com>
2026-05-01 02:46:09 -0300

    Implement Podcast class with classification and details

--- src/main/java/br/com/alura/models/Podcast.java
+++ src/main/java/br/com/alura/models/Podcast.java
@@ -1,4 +1,37 @@
 package br.com.alura.models;
 
-public class Podcast {
+public class Podcast extends Audio implements Classifiable {
+    private String apresentador;
+    private String descricao;
+
+    @Override
+    public int getClassificacao() {
+        if (this.getTotalCurtidas() > 500){
+            return 10;
+        } else {
+            return 8;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Podcast: " + this.getTitulo() + " | Apresentador: " + this.getApresentador() +
+                " | Classificação: " + this.getClassificacao() + " | Curtidas: " + this.getTotalCurtidas();
+    }
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String host) {
+        this.apresentador = host;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
